c043f0241a5553f864fe690818044d9467196694
xtesl <146335490+xtesl@users.noreply.github.com>
2024-03-28 20:30:47 +0000

    changed settings.py

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.232.129', 'localhost' 'postingbot.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
